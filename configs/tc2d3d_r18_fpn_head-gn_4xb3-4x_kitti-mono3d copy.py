7c2ea363df23f5f71f47b7f85f1c5e7328921825
josugoar <josugoar@gmail.com>
2024-06-03 06:31:15 +0200

    Add FCOS3D R101-caffe-dcn-fpn-head-gn 4xb3-4x kitti-mono3d configuration file and TC2D3D R18-fpn-head-gn 4xb3-4x kitti-mono3d copy configuration file

---
+++ configs/tc2d3d_r18_fpn_head-gn_4xb3-4x_kitti-mono3d copy.py
@@ -0,0 +1,10 @@
+_base_ = './tc2d3d_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'
+
+# model settings
+model = dict(
+    backbone=dict(
+        depth=18,
+        style='pytorch',
+        init_cfg=dict(checkpoint='torchvision://resnet18'),
+        dcn=None),
+    neck=dict(in_channels=[64, 128, 256, 512]))
