3960ec53724ed546625d30f9cdae20948377ee0d
josugoar <josugoar@gmail.com>
2024-06-05 01:11:48 +0200

    Update base configuration file import

--- configs/tc2d3d_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
+++ configs/tc2d3d_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
@@ -1,4 +1,4 @@
-_base_ = './fcos3d_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'
+_base_ = './fcos3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'  # noqa: E501
 
 custom_imports = dict(imports=['projects.TC2D3D.tc2d3d'])
 
