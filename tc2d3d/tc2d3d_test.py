f597b5a6bcb498e5eaee85cabecf9b8a7d25ae67
josugoar <josugoar@gmail.com>
2024-06-06 05:01:47 +0200

    Refactor TC2D3DTest.predict method to use box_type_3d and update origin

--- tc2d3d/tc2d3d_test.py
+++ tc2d3d/tc2d3d_test.py
@@ -14,7 +14,7 @@ class TC2D3DTest(Base3DDetector):
         raise NotImplementedError
 
     def predict(self, batch_inputs, batch_data_samples):
-        result_list_3d = []
+        results = []
 
         for batch_data_sample in batch_data_samples:
             bboxes_3d = batch_data_sample.eval_ann_info['gt_bboxes_3d']
@@ -23,6 +23,7 @@ class TC2D3DTest(Base3DDetector):
                 dtype=torch.long)
             scores_3d = bboxes_3d.tensor.new_ones(labels_3d.shape)
             cam2img = batch_data_sample.metainfo['cam2img']
+            box_type_3d = batch_data_sample.metainfo['box_type_3d']
 
             bboxes = bboxes_3d.tensor.new_tensor(
                 box3d_to_bbox(bboxes_3d.tensor.numpy(force=True), cam2img))
@@ -31,16 +32,19 @@ class TC2D3DTest(Base3DDetector):
                 bboxes_3d.dims,
                 bboxes_3d.yaw,
                 cam2img,
-                origin=(0.5, 1.0, 0.5))
+                origin=(0.5, 0.5, 0.5))
 
             result = InstanceData()
-            result.bboxes_3d = bboxes_3d
+            result.bboxes_3d = box_type_3d(
+                bboxes_3d.tensor,
+                box_dim=bboxes_3d.box_dim,
+                with_yaw=bboxes_3d.with_yaw,
+                origin=(0.5, 0.5, 0.5))
             result.labels_3d = labels_3d
             result.scores_3d = scores_3d
-            result_list_3d.append(result)
+            results.append(result)
 
-        predictions = self.add_pred_to_datasample(batch_data_samples,
-                                                  result_list_3d)
+        predictions = self.add_pred_to_datasample(batch_data_samples, results)
 
         return predictions
 
