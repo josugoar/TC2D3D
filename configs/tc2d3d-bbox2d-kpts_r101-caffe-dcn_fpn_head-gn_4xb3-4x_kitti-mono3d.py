470e021276267dfcae9f979749a7bb5a5b6fab19
josugoar <josugoar@gmail.com>
2024-06-06 05:01:12 +0200

    Rename configs

--- configs/tc2d3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
+++ configs/tc2d3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
@@ -1,11 +1,8 @@
 _base_ = './fcos3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'  # noqa: E501
 
-custom_imports = dict(imports=['projects.TC2D3D.tc2d3d'])
-
 # model settings
 model = dict(
     bbox_head=dict(
-        type='TC2D3DHead',
         reg_branch=(
             (),  # offset
             (),  # depth
@@ -13,7 +10,8 @@ model = dict(
             (256, ),  # rot
             (256, ),  # kpts
             (256, )  # bbox2d
-        )),
+        ),
+        use_tight_constraint=True),
     train_cfg=dict(code_weight=[
         0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
         0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 1.0, 1.0, 1.0, 1.0
@@ -45,6 +43,24 @@ test_pipeline = [
     dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
     dict(type='Resize3D', scale=(1242, 375), keep_ratio=True),
     dict(type='Pack3DDetInputs', keys=['img'])
+]
+tta_pipeline = [
+    dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
+    dict(
+        type='LoadAnnotations3D',
+        with_bbox=True,
+        with_label=True,
+        with_attr_label=False,
+        with_bbox_3d=True,
+        with_label_3d=True,
+        with_bbox_depth=True),
+    dict(
+        type='TestTimeAug',
+        transforms=[[
+            dict(type='Resize3D', scale=scale, keep_ratio=True)
+            for scale in [(828, 250), (1242, 375), (1863, 563)]
+        ], [dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5)],
+                    [dict(type='Pack3DDetInputs', keys=['img'])]])
 ]
 
 train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
