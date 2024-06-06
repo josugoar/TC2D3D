47a42b968fce7b6a2eb09b21ffce376f5774df56
josugoar <josugoar@gmail.com>
2024-06-06 05:01:52 +0200

    Refactor bbox_to_box3d function

--- tc2d3d/box_ops.py
+++ tc2d3d/box_ops.py
@@ -1,62 +1,72 @@
+import numpy as np
 import torch
 from mmdet.structures.bbox import bbox_overlaps
 
-from mmdet3d.structures import box3d_to_bbox, corners_nd, rotation_3d_in_axis
+from mmdet3d.structures import box3d_to_bbox, center_to_corner_box3d
 from mmdet3d.utils import array_converter
+
+CORNERS = [
+    (4, 0, 1, 6), (4, 0, 2, 6), (7, 0, 1, 6), (7, 0, 2, 6), (0, 1, 5, 7),
+    (0, 1, 6, 7), (3, 1, 5, 7), (3, 1, 6, 7), (5, 4, 0, 2), (5, 4, 3, 2),
+    (6, 4, 0, 2), (6, 4, 3, 2), (1, 5, 4, 3), (1, 5, 7, 3), (2, 5, 4, 3),
+    (2, 5, 7, 3), (1, 0, 4, 3), (1, 0, 7, 3), (2, 0, 4, 3), (2, 0, 7, 3),
+    (5, 1, 0, 2), (5, 1, 3, 2), (6, 1, 0, 2), (6, 1, 3, 2), (0, 4, 5, 7),
+    (0, 4, 6, 7), (3, 4, 5, 7), (3, 4, 6, 7), (4, 5, 1, 6), (4, 5, 2, 6),
+    (7, 5, 1, 6), (7, 5, 2, 6), (0, 1, 4, 3), (0, 5, 4, 3), (0, 1, 4, 7),
+    (0, 5, 4, 7), (1, 4, 0, 2), (1, 5, 0, 2), (1, 4, 0, 3), (1, 5, 0, 3),
+    (4, 0, 5, 6), (4, 1, 5, 6), (4, 0, 5, 7), (4, 1, 5, 7), (5, 0, 1, 2),
+    (5, 4, 1, 2), (5, 0, 1, 6), (5, 4, 1, 6), (0, 0, 4, 3), (0, 0, 4, 7),
+    (0, 4, 4, 3), (0, 4, 4, 7), (1, 1, 0, 2), (1, 1, 0, 2), (1, 0, 0, 3),
+    (1, 0, 0, 3), (4, 4, 5, 6), (4, 4, 5, 6), (4, 5, 5, 7), (4, 5, 5, 7),
+    (5, 5, 1, 2), (5, 5, 1, 2), (5, 1, 1, 6), (5, 1, 1, 6)
+]  # 4x4x4=64
 
 
 @array_converter(apply_to=('bboxes', 'dims', 'yaw', 'cam2img'))
-def bbox_to_box3d(bboxes, dims, yaw, cam2img, origin=0.5):
-    N = bboxes.shape[0]
+def bbox_to_box3d(bboxes,
+                  dims,
+                  yaw,
+                  cam2img,
+                  use_iou_constraint=False,
+                  origin=(0.5, 0.5, 0.5)):
+    batch = bboxes.shape[0]
 
-    P = cam2img
-    # TODO: combine corners an rotation with the other corner function
-    # center_to_corner_box3d
-    X = bboxes.new_tensor(corners_nd(dims.numpy(force=True), origin=origin))
-    RX = torch.eye(4, dtype=bboxes.dtype, device=bboxes.device)
-    RX = RX.reshape(1, 1, 4, 4).repeat(N, 8, 1, 1)
-    RX[:, :, :3, 3] = rotation_3d_in_axis(X, yaw, axis=1)
-    M = P @ RX
+    proj_mat = torch.eye(
+        4, dtype=bboxes.dtype,
+        device=bboxes.device).reshape(1, 1, 4, 4).repeat(batch, 8, 1, 1)
+    proj_mat[:, :, :3, 3] = bboxes.new_tensor(
+        center_to_corner_box3d(
+            np.zeros((batch, 3)),
+            dims.numpy(force=True),
+            yaw.numpy(force=True),
+            origin=origin))
+    proj_mat = cam2img @ proj_mat
 
-    T_best = bboxes.new_zeros((N, 3))
-    residual_best = bboxes.new_full((N, 1), torch.inf)
-    iou_best = bboxes.new_full((N, ), -torch.inf)
+    location_best = bboxes.new_zeros((batch, 3))
+    threshold_best = bboxes.new_full((batch, 1), torch.inf)
+    if use_iou_constraint:
+        threshold_best *= -1
 
-    for idxs in combinations:
-        A = M[:, idxs,
-              [0, 1, 0, 1], :3] - M[:, idxs, 2, :3] * bboxes[:, :, None]
-        b = M[:, idxs, 2, 3] * bboxes - M[:, idxs, [0, 1, 0, 1], 3]
+    for corners in CORNERS:
+        A = proj_mat[:, corners,
+                     [0, 1, 0, 1], :3] - proj_mat[:, corners,
+                                                  2, :3] * bboxes[:, :, None]
+        b = proj_mat[:, corners, 2, 3] * bboxes - proj_mat[:, corners,
+                                                           [0, 1, 0, 1], 3]
+        location, residual, _, _ = torch.linalg.lstsq(A, b, driver='gels')
 
-        T, residual, _, _ = torch.linalg.lstsq(A, b, driver='gels')
+        if use_iou_constraint:
+            bboxes_3d = torch.cat([location, dims, yaw], dim=1)
+            proj_bboxes = bboxes_3d.new_tensor(
+                box3d_to_bbox(bboxes_3d.numpy(force=True), cam2img))
+            iou = bbox_overlaps(proj_bboxes, bboxes, is_aligned=True)
+            mask = iou > threshold_best
+            location_best[mask] = location[mask]
+            threshold_best[mask] = iou[mask]
+        else:
+            mask = (residual < threshold_best).squeeze(-1) & (
+                location[:, 2] > 0)
+            location_best[mask] = location[mask]
+            threshold_best[mask] = residual[mask]
 
-        # TODO: project and check if inside image
-        mask = (residual < residual_best).squeeze(-1) & (T[:, 2] > 0)
-        T_best[mask] = T[mask]
-        residual_best[mask] = residual[mask]
-
-        # bboxes_3d[:, :3] = T
-        # bboxes_2d = bboxes_3d.new_tensor(
-        #     box3d_to_bbox(bboxes_3d.numpy(force=True), cam2img))
-        # iou = bbox_overlaps(bboxes_2d, bboxes, is_aligned=True)
-        # mask = iou > iou_best
-        # T_best[mask] = T[mask]
-        # iou_best[mask] = iou[mask]
-
-    return torch.cat([T_best, dims, yaw], dim=1)
-
-
-combinations = ((4, 0, 1, 6), (4, 0, 2, 6), (7, 0, 1, 6), (7, 0, 2, 6),
-                (0, 1, 5, 7), (0, 1, 6, 7), (3, 1, 5, 7), (3, 1, 6, 7),
-                (5, 4, 0, 2), (5, 4, 3, 2), (6, 4, 0, 2), (6, 4, 3, 2),
-                (1, 5, 4, 3), (1, 5, 7, 3), (2, 5, 4, 3), (2, 5, 7, 3),
-                (1, 0, 4, 3), (1, 0, 7, 3), (2, 0, 4, 3), (2, 0, 7, 3),
-                (5, 1, 0, 2), (5, 1, 3, 2), (6, 1, 0, 2), (6, 1, 3, 2),
-                (0, 4, 5, 7), (0, 4, 6, 7), (3, 4, 5, 7), (3, 4, 6, 7),
-                (4, 5, 1, 6), (4, 5, 2, 6), (7, 5, 1, 6), (7, 5, 2, 6),
-                (0, 1, 4, 3), (0, 5, 4, 3), (0, 1, 4, 7), (0, 5, 4, 7),
-                (1, 4, 0, 2), (1, 5, 0, 2), (1, 4, 0, 3), (1, 5, 0, 3),
-                (4, 0, 5, 6), (4, 1, 5, 6), (4, 0, 5, 7), (4, 1, 5, 7),
-                (5, 0, 1, 2), (5, 4, 1, 2), (5, 0, 1, 6), (5, 4, 1, 6), (0, 0, 4, 3),
-                (0, 0, 4, 7), (0, 4, 4, 3), (0, 4, 4, 7), (1, 1, 0, 2), (1, 1, 0, 2),
-                (1, 0, 0, 3), (1, 0, 0, 3), (4, 4, 5, 6), (4, 4, 5, 6), (4, 5, 5, 7),
-                (4, 5, 5, 7), (5, 5, 1, 2), (5, 5, 1, 2), (5, 1, 1, 6), (5, 1, 1, 6))
+    return torch.cat([location_best, dims, yaw], dim=1)
