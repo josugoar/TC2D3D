3bc4ddd568f041f79be8354e92d3d8edb79a266a
josugoar <josugoar@gmail.com>
2024-06-09 03:23:48 +0200

    Fix TTA and test pipelines!!! (scale and flip)

--- configs/tc2d3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
+++ configs/tc2d3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
@@ -4,7 +4,7 @@ _base_ = './fcos3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.p
 model = dict(
     bbox_head=dict(
         reg_branch=(
-            (),  # offset
+            (256, ),  # offset
             (),  # depth
             (256, ),  # size
             (256, ),  # rot
@@ -13,7 +13,7 @@ model = dict(
         ),
         use_tight_constraint=True),
     train_cfg=dict(code_weight=[
-        0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
+        1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
         0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 1.0, 1.0, 1.0, 1.0
     ]))
 
@@ -41,26 +41,8 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
-    dict(type='Resize3D', scale=(1242, 375), keep_ratio=True),
+    dict(type='Resize', scale_factor=1.0),
     dict(type='Pack3DDetInputs', keys=['img'])
-]
-tta_pipeline = [
-    dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
-    dict(
-        type='LoadAnnotations3D',
-        with_bbox=True,
-        with_label=True,
-        with_attr_label=False,
-        with_bbox_3d=True,
-        with_label_3d=True,
-        with_bbox_depth=True),
-    dict(
-        type='TestTimeAug',
-        transforms=[[
-            dict(type='Resize3D', scale=scale, keep_ratio=True)
-            for scale in [(828, 250), (1242, 375), (1863, 563)]
-        ], [dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5)],
-                    [dict(type='Pack3DDetInputs', keys=['img'])]])
 ]
 
 train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
