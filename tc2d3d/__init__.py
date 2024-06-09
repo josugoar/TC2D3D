40f43281ca4ff5e70e068b7bca39575a37a1ae4a
josugoar <josugoar@gmail.com>
2024-06-09 03:25:56 +0200

    Add BottomCenterToCenters2DWithDepth transform

--- tc2d3d/__init__.py
+++ tc2d3d/__init__.py
@@ -1,6 +1,9 @@
 from .monodet3d_tta import MonoDet3DTTAModel
 from .tc2d3d_head import TC2D3DHead
 from .tc2d3d_test import TC2D3DTest
-from .transforms_3d import BBoxes3DToBBoxes
+from .transforms_3d import BBoxes3DToBBoxes, BottomCenterToCenters2DWithDepth
 
-__all__ = ['MonoDet3DTTAModel', 'TC2D3DHead', 'TC2D3DTest', 'BBoxes3DToBBoxes']
+__all__ = [
+    'MonoDet3DTTAModel', 'TC2D3DHead', 'TC2D3DTest', 'BBoxes3DToBBoxes',
+    'BottomCenterToCenters2DWithDepth'
+]
--- tc2d3d/transforms_3d.py
+++ tc2d3d/transforms_3d.py
@@ -1,7 +1,7 @@
 from mmcv.transforms import BaseTransform
 
 from mmdet3d.registry import TRANSFORMS
-from mmdet3d.structures import box3d_to_bbox
+from mmdet3d.structures import box3d_to_bbox, points_cam2img
 
 
 @TRANSFORMS.register_module()
@@ -15,5 +15,21 @@ class BBoxes3DToBBoxes(BaseTransform):
             box3d_to_bbox(bboxes_3d.tensor.numpy(force=True), cam2img))
 
         input_dict['gt_bboxes'] = bboxes
+
+        return input_dict
+
+
+@TRANSFORMS.register_module()
+class BottomCenterToCenters2DWithDepth(BaseTransform):
+
+    def transform(self, input_dict: dict) -> dict:
+        bboxes_3d = input_dict['gt_bboxes_3d']
+        cam2img = input_dict['cam2img']
+
+        centers_2d_with_depth = points_cam2img(
+            bboxes_3d.bottom_center, cam2img, with_depth=True)
+
+        input_dict['centers_2d'] = centers_2d_with_depth[:, :2]
+        input_dict['depths'] = centers_2d_with_depth[:, 2]
 
         return input_dict
