1ee9c123ba233a5d0592ad68540f9198bbaaef6b
josugoar <josugoar@gmail.com>
2024-06-09 03:22:22 +0200

    Fix TTA model, NMS across preds

--- tc2d3d/__init__.py
+++ tc2d3d/__init__.py
@@ -1,5 +1,6 @@
+from .monodet3d_tta import MonoDet3DTTAModel
 from .tc2d3d_head import TC2D3DHead
 from .tc2d3d_test import TC2D3DTest
 from .transforms_3d import BBoxes3DToBBoxes
 
-__all__ = ['TC2D3DHead', 'TC2D3DTest', 'BBoxes3DToBBoxes']
+__all__ = ['MonoDet3DTTAModel', 'TC2D3DHead', 'TC2D3DTest', 'BBoxes3DToBBoxes']
