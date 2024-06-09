69cb99b759304bd9a8ad288ba5878c5888e077ec
josugoar <josugoar@gmail.com>
2024-06-09 03:21:32 +0200

    Rename config

--- configs/tc2d3d-bbox2d-kpts_r18-pytorch_fpn_head-gn_4xb3-4x_kitti-mono3d.py
+++ configs/tc2d3d-bbox2d-kpts_r18-pytorch_fpn_head-gn_4xb3-4x_kitti-mono3d.py
@@ -1,4 +1,4 @@
-_base_ = './tc2d3d_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'
+_base_ = './tc2d3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py'  # noqa: E501
 
 # model settings
 model = dict(
@@ -7,4 +7,4 @@ model = dict(
         style='pytorch',
         init_cfg=dict(checkpoint='torchvision://resnet18'),
         dcn=None),
-    neck=dict(in_channels=(64, 128, 256, 512)))
+    neck=dict(in_channels=[64, 128, 256, 512]))
