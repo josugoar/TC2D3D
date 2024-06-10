b3e37c2a2063799fd199c5d6b66fc95be615874e
josugoar <josugoar@gmail.com>
2024-06-11 01:44:01 +0200

    Add bbox3d_flip transform function

--- tc2d3d/__init__.py
+++ tc2d3d/__init__.py
@@ -2,8 +2,9 @@ from .monodet3d_tta import MonoDet3DTTAModel
 from .tc2d3d_head import TC2D3DHead
 from .tc2d3d_test import TC2D3DTest
 from .transforms_3d import BBoxes3DToBBoxes, BottomCenterToCenters2DWithDepth
+from .transforms import bbox3d_flip
 
 __all__ = [
     'MonoDet3DTTAModel', 'TC2D3DHead', 'TC2D3DTest', 'BBoxes3DToBBoxes',
-    'BottomCenterToCenters2DWithDepth'
+    'BottomCenterToCenters2DWithDepth', 'bbox3d_flip'
 ]
